108611c1c0b513f55c581820004903666196aac2
Aaron Elkiss <aelkiss@umich.edu>
2022-11-10 16:33:17 -0500

    Use validateCache from hathitrust/feed
    - save schema here
    - avoid having to download them several times in specs
    - won't work on arm (not currently building multiarch feed). Could fall
      back to StdInParse?

--- spec/oai_solr_spec.rb
+++ spec/oai_solr_spec.rb
@@ -34,7 +34,7 @@ RSpec.describe "OAISolr" do
     it "returns valid xml according to the OAI schema" do
       Dir.mktmpdir do |tmpdir|
         File.write("#{tmpdir}/last_response.xml", last_response.body)
-        expect(system("StdInParse -f -n -s -v=always < #{tmpdir}/last_response.xml")).to be true
+        expect(system("validateCache #{__dir__}/../config/schema.cache #{tmpdir}/last_response.xml > /dev/null")).to be true
       end
     end
   end
